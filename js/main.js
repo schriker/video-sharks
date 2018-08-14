6e5885161f5c11522ee9f624f740be5bb4887bee
schriker <janekmachine@gmail.com>
2018-08-14 09:43:32 +0200

    CustomerCarousel

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 $(function() {
+    // Pricing Boxes
     var pricing = $(".pricing");
+
     pricing.on("mouseover", function(){
         pricing.removeClass("pricing--active");
         $(this).addClass("pricing--active");
-    })
+    });
+    // Carousel Setup
+    $(".customers").owlCarousel({
+        items: 1,
+        loop: false,
+        autoplay: false,
+        dotsContainer: '.customers__dots'
+    });
+
+    $(".owl-item").addClass("col");
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+
+    $(".next").click(function(){
+        owl.trigger('next.owl.carousel');
+    });
+    
+    $(".prev").click(function(){
+        owl.trigger('prev.owl.carousel');
+});
 });
